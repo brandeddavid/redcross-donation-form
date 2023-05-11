abdc4dddc9702d846ca844ab9b6c340ec4d0d117
David Mwangi <david.mathenge98@gmail.com>
2023-05-11 14:12:02 +0300

    add FormHeader component

---
+++ src/components/FormHeader.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type Props = {
+	step: number;
+};
+
+const FormHeader = ({ step }: Props) => {
+	const getHeader = (step: number) => {
+		switch (step) {
+			case 1:
+				return "Make a Donation";
+		}
+	};
+
+	return (
+		<div className="border-0 border-b border-blue-900 border-solid">
+			<h1 className="text-3xl text-[#dc1a22]">{`Step ${step}`}</h1>
+			<h1 className="mt-5 mb-5 text-3xl">{getHeader(step)}</h1>
+		</div>
+	);
+};
+
+export default FormHeader;
