f8d39547705955df9a4338bf0688f64002984e07
David Mwangi <david.mathenge98@gmail.com>
2023-05-15 19:22:33 +0300

    add theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Button from "@mui/material/Button";
 import SelectDropdown from "@/components/SelectDropdown";
 import { SelectChangeEvent } from "@mui/material/Select";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 export default function Home() {
 	const causeOptions = [
@@ -23,41 +24,63 @@ export default function Home() {
 	const onCauseSelect = (event: SelectChangeEvent) => {
 		setSelectedCause(event.target.value);
 	};
+	const handleStepChange = (event) => {
+		setStep(step + 1);
+	};
+
+	const theme = createTheme({
+		palette: {
+			primary: {
+				main: "#dc1a22",
+			},
+		},
+	});
 
 	return (
-		<main className="flex w-full min-h-screen">
-			<div className="flex-1 hidden md:flex">
-				<div className="relative w-full h-full">
-					<Image
-						src={`https://brandeddavid.s3.eu-west-1.amazonaws.com/public/${
-							selectedCause || "maasai"
-						}.jpg`}
-						alt=""
-						fill
-					/>
-				</div>
-			</div>
-			<div className="flex flex-col justify-center flex-1 text-center bg-white space-y-[50px] md:px-5">
-				<div>
-					<h1 className="text-5xl text-[#dc1a22]">Support our Cause</h1>
-					<h3 className="text-2xl">
-						Donate today to support humanitarian work around Kenya.
-					</h3>
-				</div>
+		<ThemeProvider theme={theme}>
+			<main className="flex w-full min-h-screen">
+				<div className="flex-1 hidden md:flex">
+					<div className="relative w-full h-full">
+						<Image
+							src={`https://brandeddavid.s3.eu-west-1.amazonaws.com/public/${
+								selectedCause || "maasai"
+							}.jpg`}
+							alt=""
+							fill
+						/>
+					</div>
+				</div>{" "}
+				<div className="flex flex-col justify-center flex-1 text-center bg-white space-y-[50px] md:px-5">
+					{step === 0 && (
+						<>
+							<div>
+								<h1 className="text-5xl text-[#dc1a22]">Support our Cause</h1>
+								<h3 className="text-2xl">
+									Donate today to support humanitarian work around Kenya.
+								</h3>
+							</div>
 
-				<div>
-					<SelectDropdown
-						dropDownOptions={causeOptions}
-						selectedOption={selectedCause}
-						onChange={onCauseSelect}
-					/>
+							<div>
+								<SelectDropdown
+									dropDownOptions={causeOptions}
+									selectedOption={selectedCause}
+									onChange={onCauseSelect}
+								/>
+							</div>
+							<div>
+								<Button
+									className=""
+									variant="outlined"
+									onClick={handleStepChange}
+								>
+									Continue
+								</Button>
+							</div>
+						</>
+					)}
+					{step === 1 && <h1>Hello</h1>}{" "}
 				</div>
-				<div>
-					<Button className="" variant="outlined">
-						Continue
-					</Button>
-				</div>
-			</div>
-		</main>
+			</main>
+		</ThemeProvider>
 	);
 }
