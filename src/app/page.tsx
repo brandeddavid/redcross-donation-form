a9f40641b927484940c24d60ca8099b5c1e948fc
David Mwangi <david.mathenge98@gmail.com>
2023-05-15 17:51:23 +0300

    add RadioButton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,11 @@
 import Image from "next/image";
 import { useState } from "react";
 import Button from "@mui/material/Button";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
 import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
+import RadioButton from "@/components/RadioButton";
 
 export default function Home() {
 	const [step, setStep] = useState(1);
@@ -25,72 +22,36 @@ export default function Home() {
 					/>
 				</div>
 			</div>
-			<div className="flex flex-col justify-center flex-1 text-center bg-white space-y-50">
-				<div className="space-y-5">
+			<div className="flex flex-col justify-center flex-1 text-center bg-white space-y-[50px] md:px-5">
+				<div>
 					<h1 className="text-5xl text-[#dc1a22]">Support our Cause</h1>
 					<h3 className="text-2xl">
 						Donate today to support humanitarian work around Kenya.
 					</h3>
-					<FormControl className="justify-start">
-						<FormLabel id="demo-row-radio-buttons-group-label">
-							Select donate as
-						</FormLabel>
-						<RadioGroup
-							row
-							aria-labelledby="demo-row-radio-buttons-group-label"
-							name="row-radio-buttons-group"
+				</div>
+
+				<div>
+					<FormControl className="w-[400px] outline-[#dc1a22]">
+						<InputLabel id="demo-simple-select-label">
+							Select a cause
+						</InputLabel>
+						<Select
+							labelId="demo-simple-select-label"
+							id="demo-simple-select"
+							value=""
+							label="Select a cause"
+							onChange={() => {}}
 						>
-							<FormControlLabel
-								value="Individual"
-								control={
-									<Radio
-										sx={{
-											"&.Mui-checked": {
-												color: "#dc1a22",
-											},
-										}}
-									/>
-								}
-								label="Individual"
-							/>
-							<FormControlLabel
-								value="Company"
-								control={
-									<Radio
-										sx={{
-											"&.Mui-checked": {
-												color: "#dc1a22",
-											},
-										}}
-									/>
-								}
-								label="Company"
-							/>
-						</RadioGroup>
+							<MenuItem value={10}>Ten</MenuItem>
+							<MenuItem value={20}>Twenty</MenuItem>
+							<MenuItem value={30}>Thirty</MenuItem>
+						</Select>
 					</FormControl>
-					<div>
-						<FormControl className="w-[400px] outline-[#dc1a22]">
-							<InputLabel id="demo-simple-select-label">
-								Select a cause
-							</InputLabel>
-							<Select
-								labelId="demo-simple-select-label"
-								id="demo-simple-select"
-								value=""
-								label="Select a cause"
-								onChange={() => {}}
-							>
-								<MenuItem value={10}>Ten</MenuItem>
-								<MenuItem value={20}>Twenty</MenuItem>
-								<MenuItem value={30}>Thirty</MenuItem>
-							</Select>
-						</FormControl>
-					</div>
-					<div>
-						<Button className="" variant="outlined">
-							Donate
-						</Button>
-					</div>
+				</div>
+				<div>
+					<Button className="" variant="outlined">
+						Donate
+					</Button>
 				</div>
 			</div>
 		</main>
