6bee3409fa1a23e1e6aeb18e0acdd979eedd5caf
David Mwangi <david.mathenge98@gmail.com>
2023-05-16 10:07:42 +0300

    add tabs and currency buttons

--- src/components/DonationForm.tsx
+++ src/components/DonationForm.tsx
@@ -1,13 +1,30 @@
-import React from "react";
-import { Divider, StepLabel, Step, Stepper, Card } from "@mui/material";
+import React, { useState } from "react";
+import {
+	Divider,
+	StepLabel,
+	Step,
+	Stepper,
+	Card,
+	Tabs,
+	Tab,
+	Button,
+	ButtonGroup,
+} from "@mui/material";
+import RadioButton from "./RadioButton";
 
 type Props = {
 	step: number;
 };
 
 const steps = ["Cause", "Donation", "Details"];
+const tabs = [
+	{ label: "ONE TIME", value: "one-time" },
+	{ label: "MONTHLY", value: "monthly" },
+];
+const acceptedCurrency = ["KSH", "USD"];
 
 function DonationForm({ step }: Props) {
+	const [selectedCurrency, setSelectedCurrency] = useState("KSH");
 	return (
 		<div className="w-full">
 			<Card variant="outlined">
@@ -15,7 +32,7 @@ function DonationForm({ step }: Props) {
 					activeStep={step}
 					alternativeLabel
 					// orientation="vertical"
-					className="h-[50px] p-[10px] mb-[20px]"
+					className="px-[10px] py-[20px]"
 				>
 					{steps.map((label) => (
 						<Step key={label}>
@@ -24,6 +41,45 @@ function DonationForm({ step }: Props) {
 					))}
 				</Stepper>
 				<Divider />
+				<div className="p-[30px] flex flex-col space-y-[20px]">
+					<RadioButton
+						formLabel="Select donate as"
+						radioOptions={[
+							{ label: "Individual", value: "individual" },
+							{ label: "Company", value: "company" },
+						]}
+					/>
+					<RadioButton
+						formLabel="Select payment option"
+						radioOptions={[
+							{ label: "Pay Now", value: "pay-now" },
+							{ label: "Make Pledge", value: "make-pledge" },
+						]}
+					/>
+					<div className="flex justify-center">
+						<Tabs value={0} onChange={() => {}} aria-label="basic tabs example">
+							{tabs.map(({ label, value }) => (
+								<Tab key={label} label={label} />
+							))}
+						</Tabs>
+					</div>
+					<div className="flex justify-end">
+						<ButtonGroup variant="outlined" aria-label="button group">
+							{acceptedCurrency.map((currency) => (
+								<Button
+									key={currency}
+									className={`${
+										(currency === selectedCurrency &&
+											"bg-[#dc1a22] text-white") ||
+										""
+									}`}
+								>
+									{currency}
+								</Button>
+							))}
+						</ButtonGroup>
+					</div>
+				</div>
 			</Card>
 		</div>
 	);
