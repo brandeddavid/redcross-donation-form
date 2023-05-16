b710290977c39c11209b44c13493ca328767bf01
David Mwangi <david.mathenge98@gmail.com>
2023-05-16 19:08:24 +0300

    fix placement bugs

--- src/components/DonationForm.tsx
+++ src/components/DonationForm.tsx
@@ -22,36 +22,41 @@ const steps = ["Cause", "Amount", "Donate"];
 function DonationForm({ step, onContinue, onBack }: Props) {
 	return (
 		<div className="flex flex-col justify-between w-full md:w-[400px] lg:w-[600px]">
-			<Stepper
-				activeStep={step}
-				alternativeLabel
-				// orientation="vertical"
-				className="px-[10px] py-[20px]"
-			>
-				{steps.map((label) => (
-					<Step key={label}>
-						<StepLabel>{label}</StepLabel>
-					</Step>
-				))}
-			</Stepper>
-			<Divider />
+			<div>
+				<Stepper
+					activeStep={step}
+					alternativeLabel
+					// orientation="vertical"
+					className="px-[10px] py-[20px]"
+				>
+					{steps.map((label) => (
+						<Step key={label}>
+							<StepLabel>{label}</StepLabel>
+						</Step>
+					))}
+				</Stepper>
+				<Divider />
+			</div>
+
 			<div className="min-h-[400px]">
 				{step === 1 && <DonationFormAmount />}
 				{step === 2 && <DonationFormPersonalDetails />}
 			</div>
 
-			<Divider />
-			<CardActions className="flex justify-between p-5">
-				<Button className="px-[10px]" variant="outlined" onClick={onBack}>
-					Back
-				</Button>
-				<Button
-					className="px-[10px] bg-[#dc1a22] text-white hover:bg-[#dc1a22]"
-					onClick={onContinue}
-				>
-					Continue
-				</Button>
-			</CardActions>
+			<div>
+				<Divider />
+				<CardActions className="flex justify-between p-5">
+					<Button className="px-[10px]" variant="outlined" onClick={onBack}>
+						Back
+					</Button>
+					<Button
+						className="px-[10px] bg-[#dc1a22] text-white hover:bg-[#dc1a22]"
+						onClick={onContinue}
+					>
+						Continue
+					</Button>
+				</CardActions>
+			</div>
 		</div>
 	);
 }
