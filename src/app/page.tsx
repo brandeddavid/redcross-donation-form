cebc948922f5aeae67d4a3ad1796f752f6d1c8fe
David Mwangi <david.mathenge98@gmail.com>
2023-05-16 19:00:09 +0300

    add text on left section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,31 +47,33 @@ export default function Home() {
 	return (
 		<ThemeProvider theme={theme}>
 			<main className="flex w-full min-h-screen">
-				{step === 0 && (
-					<div className="hidden md:flex flex-1">
-						<div className="relative w-full h-full">
-							<Image src={`/${selectedCause || "maasai"}.jpg`} alt="" fill />
-						</div>
+				<div className="relative hidden md:flex flex-1 flex-col justify-center">
+					<div className=" w-full h-full flex justify-end ">
+						<Image
+							className="bg-[#dc1a22] opacity-20 z-10"
+							src={`/${selectedCause || "maasai"}.jpg`}
+							alt=""
+							fill
+						/>
 					</div>
-				)}
-
-				{step > 0 && (
-					<div className="hidden md:flex flex-1 flex-col justify-center">
-						<div className="relative w-full h-full flex justify-end ">
-							<div className="absolute top-[30%] h-[400px] w-auto bg-white flex flex-col text-center justify-center">
-								<div>
-									<h1 className="text-3xl">{`Support ${selectedCause}`}</h1>
-									<p className="px-5 mt-5">
-										Donate today to support humanitarian work around Kenya. In
-										times of crisis, we meet the urgent needs of women, men,
-										young and the old. Help enable a rapid response to
-										disasters. Your contribution can make a difference.
-									</p>
-								</div>
+					{selectedCause && (
+						<div className="z-20 absolute top-[30%] h-[400px] bg-[#dc1a22] w-auto text-white opacity-70 flex flex-col text-center justify-center">
+							<div>
+								<h1 className="text-3xl">{`Support ${selectedCause}`}</h1>
+								<p className="px-5 mt-5">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+									do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+									Ut enim ad minim veniam, quis nostrud exercitation ullamco
+									laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+									irure dolor in reprehenderit in voluptate velit esse cillum
+									dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+									cupidatat non proident, sunt in culpa qui officia deserunt
+									mollit anim id est laborum.
+								</p>
 							</div>
 						</div>
-					</div>
-				)}
+					)}
+				</div>
 
 				{step === 0 && (
 					<div className="flex flex-col justify-center flex-1 text-center bg-white space-y-[50px] md:px-5">
