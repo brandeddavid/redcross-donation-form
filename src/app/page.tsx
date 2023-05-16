a869563900e82d024bb46d93c7731e20763a3432
David Mwangi <david.mathenge98@gmail.com>
2023-05-16 09:01:20 +0300

    add stepper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Button from "@mui/material/Button";
 import SelectDropdown from "@/components/SelectDropdown";
 import { SelectChangeEvent } from "@mui/material/Select";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import DonationForm from "@/components/DonationForm";
 
 export default function Home() {
 	const causeOptions = [
@@ -41,45 +42,41 @@ export default function Home() {
 			<main className="flex w-full min-h-screen">
 				<div className="flex-1 hidden md:flex">
 					<div className="relative w-full h-full">
-						<Image
-							src={`https://brandeddavid.s3.eu-west-1.amazonaws.com/public/${
-								selectedCause || "maasai"
-							}.jpg`}
-							alt=""
-							fill
-						/>
+						<Image src={`/${selectedCause || "maasai"}.jpg`} alt="" fill />
 					</div>
-				</div>{" "}
-				<div className="flex flex-col justify-center flex-1 text-center bg-white space-y-[50px] md:px-5">
-					{step === 0 && (
-						<>
-							<div>
-								<h1 className="text-5xl text-[#dc1a22]">Support our Cause</h1>
-								<h3 className="text-2xl">
-									Donate today to support humanitarian work around Kenya.
-								</h3>
-							</div>
-
-							<div>
-								<SelectDropdown
-									dropDownOptions={causeOptions}
-									selectedOption={selectedCause}
-									onChange={onCauseSelect}
-								/>
-							</div>
-							<div>
-								<Button
-									className=""
-									variant="outlined"
-									onClick={handleStepChange}
-								>
-									Continue
-								</Button>
-							</div>
-						</>
-					)}
-					{step === 1 && <h1>Hello</h1>}{" "}
 				</div>
+				{step === 0 && (
+					<div className="flex flex-col justify-center flex-1 text-center bg-white space-y-[50px] md:px-5">
+						<div>
+							<h1 className="text-5xl text-[#dc1a22]">Support our Cause</h1>
+							<h3 className="text-2xl">
+								Donate today to support humanitarian work around Kenya.
+							</h3>
+						</div>
+
+						<div>
+							<SelectDropdown
+								dropDownOptions={causeOptions}
+								selectedOption={selectedCause}
+								onChange={onCauseSelect}
+							/>
+						</div>
+						<div>
+							<Button
+								className=""
+								variant="outlined"
+								onClick={handleStepChange}
+							>
+								Continue
+							</Button>
+						</div>
+					</div>
+				)}
+				{step === 1 && (
+					<div className="flex flex-col flex-1 text-center bg-white space-y-[50px] md:p-5">
+						<DonationForm step={step} />
+					</div>
+				)}
 			</main>
 		</ThemeProvider>
 	);
