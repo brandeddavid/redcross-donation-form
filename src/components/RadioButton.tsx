744fe854650a6d7333deed54b3851a9089d56cf8
David Mwangi <david.mathenge98@gmail.com>
2023-05-16 17:04:53 +0300

    addd personal details step

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import FormLabel from "@mui/material/FormLabel";
 import Radio from "@mui/material/Radio";
--- src/components/SelectDropdown.tsx
+++ src/components/SelectDropdown.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
