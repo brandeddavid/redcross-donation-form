60de6067eb82abbae9474b9070d1502c99beca6d
David Mwangi <david.mathenge98@gmail.com>
2023-05-19 12:42:43 +0300

    add bt hover property

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button as Btn } from "@mui/material/";
+import { Button as Btn, withStyles } from "@mui/material/";
 
 type Props = {
 	children: any;
@@ -7,9 +7,24 @@ type Props = {
 	variant?: "text" | "outlined" | "contained";
 };
 
+const styles = () => ({
+	default: {
+		width: "300px",
+	},
+});
+
 const Button = ({ children, onClick, variant = "outlined" }: Props) => {
 	return (
-		<Btn variant={variant} onClick={onClick}>
+		<Btn
+			variant={variant}
+			onClick={onClick}
+			sx={{
+				":hover": {
+					bgcolor: "#ed1c24",
+					color: "white",
+				},
+			}}
+		>
 			{children}
 		</Btn>
 	);
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,9 +3,23 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
 	palette: {
 		primary: {
-			main: "#dc1a22",
+			main: "#ed1c24",
+		},
+		secondary: {
+			main: "#f8f9fa",
 		},
 	},
+	// components: {
+	// 	MuiButton: {
+	// 		styleOverrides: {
+	// 			root: {
+	// 				"&hover": {
+	// 					bgcolor: "#ed1c24",
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// },
 });
 
 export { theme };
