e1358b6807d32a3af32311fe40a023fc349a6bfd
David Mwangi <david.mathenge98@gmail.com>
2023-05-21 22:29:16 +0300

    update meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "Kenya Redcross",
+	title: "Kenya Redcross | Donation Form",
 	description: "",
 };
 
