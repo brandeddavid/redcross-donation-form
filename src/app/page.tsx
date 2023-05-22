f2ddc359f02668e195d128fcb646bba5d2c5f8b7
David Mwangi <david.mathenge98@gmail.com>
2023-05-22 08:30:24 +0300

    add bg color to text section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
 	return (
 		<ThemeProvider theme={theme}>
 			<main className="flex w-full min-h-screen">
-				<div className="relative flex-col justify-center flex-1 hidden bg-gray-300 md:flex">
+				<div className="relative flex-col justify-center flex-1 hidden bg-[#f8f9fa] md:flex">
 					<div className="flex justify-end w-full h-[700px]">
 						<Image
 							className={`${step > 0 ? "opacity-20" : "opacity-50"} z-10`}
@@ -57,18 +57,14 @@ export default function Home() {
 						/>
 					</div>
 					{selectedCause && step > 0 && (
-						<div className="z-20 absolute top-[30%] h-[400px] bg-[#dc1a22] w-auto text-white opacity-70 flex flex-col text-center justify-center">
+						<div className="z-20 absolute top-[30%] h-[400px] bg-[#ed1c24] w-auto text-white opacity-70 flex flex-col text-center justify-center">
 							<div>
 								<h1 className="text-3xl">{`Support ${selectedCause}`}</h1>
 								<p className="px-5 mt-5">
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-									do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-									Ut enim ad minim veniam, quis nostrud exercitation ullamco
-									laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-									irure dolor in reprehenderit in voluptate velit esse cillum
-									dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-									cupidatat non proident, sunt in culpa qui officia deserunt
-									mollit anim id est laborum.
+									Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+									Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
+									natoque penatibus et magnis dis parturient montes, nascetur
+									ridiculus mus. Donec qu
 								</p>
 							</div>
 						</div>
@@ -77,9 +73,9 @@ export default function Home() {
 
 				{step === 0 && (
 					<div className="flex flex-col justify-center flex-1 text-center bg-white space-y-[50px] md:px-5">
-						<div>
-							<h1 className="text-5xl text-[#dc1a22]">Support our Cause</h1>
-							<p className="px-5 mt-5">
+						<div className="bg-[#ed1c24] p-[20px]">
+							<h1 className="text-5xl text-white">Support our Cause</h1>
+							<p className="px-5 mt-5 text-white">
 								Donate today to support humanitarian work around Kenya. In times
 								of crisis, we meet the urgent needs of women, men, young and the
 								old. Help enable a rapid response to disasters. Your
@@ -94,9 +90,8 @@ export default function Home() {
 								onChange={onCauseSelect}
 							/>
 							{selectedCause && (
-								<p className="text-sm text-gray-500 mt-[5px]">
-									Campaign from <strong>14 May 2023</strong> to{" "}
-									<strong>25 Dec 2023</strong>
+								<p className="text-sm text-[#ed1c24] mt-[5px]">
+									Campaign running until <strong>25 Dec 2023</strong>
 								</p>
 							)}
 						</div>
