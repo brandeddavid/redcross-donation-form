493408bc036857b616014b18f15e8edb9bb760a3
David Mwangi <david.mathenge98@gmail.com>
2023-05-24 15:31:31 +0300

    add get redcross causes endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,42 @@
 import express from "express";
+import mysql, { raw } from "mysql";
 
 const app = express();
 const port = 8800;
 
+const db = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "marigi@98",
+	database: "krc_donation",
+});
+
 app.get("/", (req, res) => {
 	res.send("Hello World!");
+});
+
+app.get("/campaigns", (req, res) => {
+	const query = "SELECT * FROM donation_type";
+
+	db.query(query, (error, data) => {
+		if (error) {
+			return res.json(error);
+		}
+
+		// const formattedData = rawData.map(
+		// 	({ id, name, description, start_date, end_date }) => {
+		// 		return {
+		// 			id,
+		// 			name,
+		// 			description,
+		// 			startDate: start_date,
+		// 			endDate: end_date,
+		// 		};
+		// 	}
+		// );
+
+		return res.json(data);
+	});
 });
 
 app.listen(port, () => {
