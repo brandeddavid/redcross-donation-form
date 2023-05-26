beb349fffcb4a918db296e1328ae8c41aa59b762
David Mwangi <david.mathenge98@gmail.com>
2023-05-26 22:08:57 +0300

    build

---
+++ .next/server/app/favicon.ico/route.js
@@ -0,0 +1,142 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 155;
+exports.ids = [155];
+exports.modules = {
+
+/***/ 97783:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/@edge-runtime/cookies");
+
+/***/ }),
+
+/***/ 28530:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/@opentelemetry/api");
+
+/***/ }),
+
+/***/ 54426:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/chalk");
+
+/***/ }),
+
+/***/ 40252:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/cookie");
+
+/***/ }),
+
+/***/ 32196:
+/***/ ((module) => {
+
+module.exports = require("next/dist/compiled/ua-parser-js");
+
+/***/ }),
+
+/***/ 14021:
+/***/ ((module) => {
+
+module.exports = import("next/dist/compiled/@vercel/og/index.node.js");;
+
+/***/ }),
+
+/***/ 84231:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "headerHooks": () => (/* binding */ headerHooks),
+  "originalPathname": () => (/* binding */ originalPathname),
+  "requestAsyncStorage": () => (/* binding */ requestAsyncStorage),
+  "routeModule": () => (/* binding */ routeModule),
+  "serverHooks": () => (/* binding */ serverHooks),
+  "staticGenerationAsyncStorage": () => (/* binding */ staticGenerationAsyncStorage),
+  "staticGenerationBailout": () => (/* binding */ staticGenerationBailout)
+});
+
+// NAMESPACE OBJECT: ./node_modules/next/dist/build/webpack/loaders/next-metadata-route-loader.js?page=%2Ffavicon.ico%2Froute&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js!./src/app/favicon.ico?__next_metadata
+var favicon_next_metadata_namespaceObject = {};
+__webpack_require__.r(favicon_next_metadata_namespaceObject);
+__webpack_require__.d(favicon_next_metadata_namespaceObject, {
+  "GET": () => (GET),
+  "dynamic": () => (dynamic)
+});
+
+// EXTERNAL MODULE: ./node_modules/next/dist/server/node-polyfill-headers.js
+var node_polyfill_headers = __webpack_require__(76145);
+// EXTERNAL MODULE: ./node_modules/next/dist/server/future/route-modules/app-route/module.js
+var app_route_module = __webpack_require__(19532);
+var module_default = /*#__PURE__*/__webpack_require__.n(app_route_module);
+// EXTERNAL MODULE: ./node_modules/next/server.js
+var server = __webpack_require__(38529);
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-metadata-route-loader.js?page=%2Ffavicon.ico%2Froute&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js!./src/app/favicon.ico?__next_metadata
+
+
+const contentType = "image/x-icon"
+const buffer = Buffer.from("AAABAAEAEBAAAAEAIABoBAAAFgAAACgAAAAQAAAAIAAAAAEAIAAAAAAAAAQAABILAAASCwAAAAAAAAAAAAD+/v7//////+zs7P93dnn///////Dw8P+2tbf/2Njb/9LQ9f/Bv/H//////+Pj4/91dXn///////////////////////39/f//////hYWH/3d2eP+GhYj/4eHg////////////y8nz/4qF+/9qaYD/p6ef///////9/fz////////////8/Pz//////6uqrP8hICT/6Ojn//n5///Z1/f/2tj5///////GxPj/IxzS/9jW9////////f3////////9/f3//////8/Pz/8TEhb/rq60//////+ppu//GhDp/x8V6v+9uvf//////4eD9v8rIuj/7u77///////+/v///v7+//////9GRUn/NTQ4//z8/v//////sa7z/xgP6/8eFOz/xcL4///////s6/r/Hxbp/3Zx7////////Pz////////Y2Nn/DQwP/29vdf+mov7/ko7w/2pk7/8fFuz/IRjs/3Rv8P+RjfP/qKXz/1dQ7f8oH+r/9PP8////////////t7a4/wYFBv+Dgo//Ny74/woA6f8bEe3/JRzt/yUb7f8aEO3/CADs/0U97f9zbu//Ewrq/9nY+v///////////7a1t/8FBAb/goGM/0pC9/8iGef/KSDr/yQb7f8kG+3/KiHr/yAX6f9WT+v/bWjv/xYM6v/e3Pr////////////e3t//EA8T/2hna//r6v//5uX3/5uX8f8dFOz/IRns/62p9P/m5fn/5uX4/0lC7f8zK+v/+/v8///////9/f3//////2JhZP8dHCD/7e3v//////+rqPL/DALq/xII6//Avvf//////9fV+f8QBun/k4/x///////8/P///v7+///////t7e7/Kiks/29uc///////wL3x/1ZP6/9ZU+v/0M/4//////9QSun/Tkfr//7+/f///////v7////////9/f3//////+Pj4/9PT1L/pKSm///////9/f3//v7///////+OivP/ZV/3//X0+////////v7///////////////////39/f//////mpmb/21sb/+8u73/+/v8//X1+/+yr/j/ZmK9/7Kxrv///////Pz9//////////////////z8/P//////nJye/2VlZ///////4+Pj/+bm5v/i4fb/6ej8//7/9v9MS07/xcXH///////9/f3//////////////////v7+/3V0dv/n5+f///////7+/v////////////39/f//////y8rL/4KChP///////f39///////+/v7//////+fn6P/Ix8j///////39/f/+/v7//v7+//7+///+/v///v7+///////AwMH/9vX2////////////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA==", 'base64'
+  )
+
+function GET() {
+  return new server.NextResponse(buffer, {
+    headers: {
+      'Content-Type': contentType,
+      'Cache-Control': "public, max-age=0, must-revalidate",
+    },
+  })
+}
+
+const dynamic = 'force-static'
+
+;// CONCATENATED MODULE: ./node_modules/next/dist/build/webpack/loaders/next-app-loader.js?page=%2Ffavicon.ico%2Froute&name=app%2Ffavicon.ico%2Froute&pagePath=private-next-app-dir%2Ffavicon.ico&appDir=%2FUsers%2Fmac%2FDocuments%2Fredcross-donation-form%2Fsrc%2Fapp&appPaths=%2Ffavicon.ico&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js&basePath=&assetPrefix=&nextConfigOutput=&preferredRegion=!
+
+    
+
+    
+
+    
+
+    const routeModule = new (module_default())({
+    userland: favicon_next_metadata_namespaceObject,
+    pathname: "/favicon.ico",
+    resolvedPagePath: "next-metadata-route-loader?page=%2Ffavicon.ico%2Froute&pageExtensions=tsx&pageExtensions=ts&pageExtensions=jsx&pageExtensions=js!/Users/mac/Documents/redcross-donation-form/src/app/favicon.ico?__next_metadata",
+    nextConfigOutput: undefined,
+  })
+
+    // Pull out the exports that we need to expose from the module. This should
+    // be eliminated when we've moved the other routes to the new format. These
+    // are used to hook into the route.
+    const {
+      requestAsyncStorage,
+      staticGenerationAsyncStorage,
+      serverHooks,
+      headerHooks,
+      staticGenerationBailout
+    } = routeModule
+
+    const originalPathname = "/favicon.ico/route"
+
+    
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [859,109], () => (__webpack_exec__(84231)));
+module.exports = __webpack_exports__;
+
+})();
