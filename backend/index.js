062d5576ee74abb5d1e51061ef0561f0f24d0af9
brandeddavid <david.mathenge98@gmail.com>
2023-05-26 22:06:16 +0000

    change welcome message

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,10 @@ const db = mysql.createConnection({
 });
 
 app.get("/", (req, res) => {
-	res.send("Welcome to Redcross Donation Form API!");
+	res.send({
+		status: 200,
+		message: "Welcome to Redcross Donation Form API!"
+	});
 });
 
 app.get("/campaigns", (req, res) => {
