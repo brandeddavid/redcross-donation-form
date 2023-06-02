a8ac6fc3637c859098b87575a8588d21ad388e80
David Mwangi <david.mathenge98@gmail.com>
2023-06-02 14:34:33 +0300

    add modal

--- src/components/DonationFormPayment.tsx
+++ src/components/DonationFormPayment.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useContext } from "react";
-import { Tabs, Tab, TextField } from "@mui/material";
+import { Tabs, Tab, TextField, Modal, Box } from "@mui/material";
 import Button from "./Button";
 import { DonationFormContext } from "../context/donationFormContext";
+import ModalContent from "./ModalContent";
 
 type Props = {};
 
@@ -16,55 +17,56 @@ const DonationFormPayment = ({}: Props) => {
 	} = useContext(DonationFormContext);
 
 	return (
-		<div className="space-y-[40px]">
-			<div className="max-w-[300px] md:max-w-none mt-[20px]">
-				<Tabs
-					value={donationFormDetails?.paymentOption}
-					variant="scrollable"
-					scrollButtons
-					allowScrollButtonsMobile
-				>
-					{paymentOptions.map((option) => (
-						<Tab
-							key={option}
-							label={option}
-							value={option}
-							onClick={() => setPaymentOption(option)}
-						/>
-					))}
-				</Tabs>
-			</div>
-			{donationFormDetails?.paymentOption === "Mpesa" && (
-				<div className="flex justify-center min-h-[250px]">
-					<div className="flex flex-col justify-center space-y-[40px] w-[300px]">
-						<TextField
-							label="Enter Mpesa Number"
-							placeholder="Enter Mpesa number"
-							variant="standard"
-							value={donationFormDetails?.phoneNumber}
-							onChange={(event) => {
-								setPhoneNumber(event.target.value);
-							}}
-						/>
-
-						<TextField
-							label="Amount"
-							placeholder="Enter amount"
-							variant="standard"
-							value={`${
-								donationFormDetails?.handleProcessingFee
-									? donationFormDetails?.totalDonationAmount
-									: donationFormDetails?.donationAmount
-							}`}
-							onChange={(event) => {
-								setDonationAmount(event.target.value);
-							}}
-						/>
-					</div>
+		<>
+			<div className="space-y-[40px]">
+				<div className="max-w-[300px] md:max-w-none mt-[20px]">
+					<Tabs
+						value={donationFormDetails?.paymentOption}
+						variant="scrollable"
+						scrollButtons
+						allowScrollButtonsMobile
+					>
+						{paymentOptions.map((option) => (
+							<Tab
+								key={option}
+								label={option}
+								value={option}
+								onClick={() => setPaymentOption(option)}
+							/>
+						))}
+					</Tabs>
 				</div>
-			)}
+				{donationFormDetails?.paymentOption === "Mpesa" && (
+					<div className="flex justify-center min-h-[250px]">
+						<div className="flex flex-col justify-center space-y-[40px] w-[300px]">
+							<TextField
+								label="Enter Mpesa Number"
+								placeholder="Enter Mpesa number"
+								variant="standard"
+								value={donationFormDetails?.phoneNumber}
+								onChange={(event) => {
+									setPhoneNumber(event.target.value);
+								}}
+							/>
 
-			{/* {donationFormDetails?.paymentOption === "Card" && (
+							<TextField
+								label="Amount"
+								placeholder="Enter amount"
+								variant="standard"
+								value={`${
+									donationFormDetails?.handleProcessingFee
+										? donationFormDetails?.totalDonationAmount
+										: donationFormDetails?.donationAmount
+								}`}
+								onChange={(event) => {
+									setDonationAmount(event.target.value);
+								}}
+							/>
+						</div>
+					</div>
+				)}
+
+				{/* {donationFormDetails?.paymentOption === "Card" && (
 				<div className="flex flex-col justify-center space-y-[40px]">
 					<Button onClick={() => {}}>{`Donate ${
 						donationFormDetails?.selectedCurrency
@@ -76,48 +78,52 @@ const DonationFormPayment = ({}: Props) => {
 				</div>
 			)} */}
 
-			{donationFormDetails?.paymentOption === "Airtel Money" && (
-				<div>
-					<p>To make payment, follow these steps:</p>
-					<div className="flex p-[20px]">
-						<ol className="text-gray-500 mt-[20px] list-outside list-decimal">
-							<li>Go to Airtel on your phone.</li>
-							<li>Select &#39;Send Money&#39;.</li>
-							<li>Enter &#39;Nick Name&#39;: Redcross.</li>
-							<li>Enter your Airtel PIN.</li>
-							<li>Enter the reference: leave it blank.</li>
-							<li>You will receive an SMS from Airtel.</li>
-						</ol>
+				{donationFormDetails?.paymentOption === "Airtel Money" && (
+					<div>
+						<p>To make payment, follow these steps:</p>
+						<div className="flex p-[20px]">
+							<ol className="text-gray-500 mt-[20px] list-outside list-decimal">
+								<li>Go to Airtel on your phone.</li>
+								<li>Select &#39;Send Money&#39;.</li>
+								<li>Enter &#39;Nick Name&#39;: Redcross.</li>
+								<li>Enter your Airtel PIN.</li>
+								<li>Enter the reference: leave it blank.</li>
+								<li>You will receive an SMS from Airtel.</li>
+							</ol>
+						</div>
 					</div>
-				</div>
-			)}
+				)}
 
-			{donationFormDetails?.paymentOption === "TKash" && (
-				<div>
-					<p>To make payment, follow these steps:</p>
-					<div className="flex p-[20px]">
-						<ol className="text-gray-500 mt-[20px] list-outside list-decimal">
-							<li>Go to Tkash menu on your phone or Use the T-kash App.</li>
-							<li>Select “Paybill”</li>
-							<li>Select “T-kash Paybill”.</li>
-							<li>Enter “Paybill Number”</li>
-							<li>Enter “Campaign”</li>
-							<li>
-								Enter{" "}
-								{`${
-									donationFormDetails?.handleProcessingFee
-										? donationFormDetails?.totalDonationAmount
-										: donationFormDetails?.donationAmount
-								}`}
-							</li>
-							<li>Confirm transaction.</li>
-							<li>Enter T-kash PIN.</li>
-							<li>You will receive a confirmation SMS from T-kash.</li>
-						</ol>
+				{donationFormDetails?.paymentOption === "TKash" && (
+					<div>
+						<p>To make payment, follow these steps:</p>
+						<div className="flex p-[20px]">
+							<ol className="text-gray-500 mt-[20px] list-outside list-decimal">
+								<li>Go to Tkash menu on your phone or Use the T-kash App.</li>
+								<li>Select “Paybill”</li>
+								<li>Select “T-kash Paybill”.</li>
+								<li>Enter “Paybill Number”</li>
+								<li>Enter “Campaign”</li>
+								<li>
+									Enter{" "}
+									{`${
+										donationFormDetails?.handleProcessingFee
+											? donationFormDetails?.totalDonationAmount
+											: donationFormDetails?.donationAmount
+									}`}
+								</li>
+								<li>Confirm transaction.</li>
+								<li>Enter T-kash PIN.</li>
+								<li>You will receive a confirmation SMS from T-kash.</li>
+							</ol>
+						</div>
 					</div>
-				</div>
-			)}
-		</div>
+				)}
+			</div>
+			<Modal open={true} onClose={() => {}}>
+				<ModalContent />
+			</Modal>
+		</>
 	);
 };
 
---
+++ src/components/ModalContent.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Box, Button } from "@mui/material";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+
+type Props = {};
+
+const ModalContent = (props: Props) => {
+	return (
+		<Box
+			sx={{
+				width: 400,
+				position: "absolute" as "absolute",
+				top: "50%",
+				left: "50%",
+				transform: "translate(-50%, -50%)",
+				bgcolor: "background.paper",
+				boxShadow: 50,
+			}}
+		>
+			<div className="h-[200px] bg-[#ed1c24] flex flex-col justify-center text-center">
+				<div>
+					<CheckCircleOutlineIcon fontSize="large" sx={{ fill: "white" }} />
+				</div>
+				<div className="text-white">SUCCESSFUL</div>
+			</div>
+			<div className="p-[50px] space-y-[40px] flex flex-col justify-center text-center">
+				<div className="text-xl">
+					Your donation has been successfully processed
+				</div>
+				<div>
+					<Button
+						className="bg-[#ed1c24] text-white"
+						sx={{ width: "200px", bgcolor: "#ed1c24" }}
+						color="primary"
+					>
+						Back
+					</Button>
+				</div>
+			</div>
+		</Box>
+	);
+};
+
+export default ModalContent;
